0907d33dd314f189cfc8aaea583b7fb935df0a24
Unknown <catarina.fvaz11@gmail.com>
2018-10-08 17:20:37 +0100

    exercicio 6
    Aulas, exercicios

---
+++ Exercicios/Exercicio6/ValidatorAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicios
+{
+    internal class ValidatorAttribute : Attribute
+    {
+        private string v;
+
+        public ValidatorAttribute(string v)
+        {
+            this.v = v;
+        }
+
+        //verifca se valor passado como parametros é superior a 20000
+        public static bool NumberGreaterThan20000(int number)
+        {
+           
+            return number>20000?  true : false;
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-AVE-1819i
+# ave-2018-19-sem1-li41n
+Recursos das aulas de AVE - Semestre de Inverno 2018/2019 - Turma LI41N
